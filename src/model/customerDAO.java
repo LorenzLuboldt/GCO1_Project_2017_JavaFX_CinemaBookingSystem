d32674f7ddc431e6e910fdd091048abd29f606a6
MichaelAring <michael.erik.aring@gmail.com>
2017-11-27 12:08:54 +0000

    Update customerDAO

--- src/model/customerDAO.java
+++ src/model/customerDAO.java
@@ -1,5 +1,21 @@
 package model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import util.SqlUtil;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class customerDAO {
+	
+	// method to receive customer information
+	public static ObservableList<customer> getCustInfo() throws SQLException, ClassNotFoundException	{
+		
+		String selector = "SELECT * FROM customers";
+		ResultSet queryResults = SqlUtil.dbExecuteQuery(selector);
+		ObservableList<customer> custList;
+		return custList;
+	}
 
 }
