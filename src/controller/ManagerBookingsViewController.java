44076e532b1d4a4b34c42c8f3d4887bb2a7b68bf
Lorenz Luboldt <lorenzluboldt@gmx.net>
2017-12-05 16:59:15 +0000

    Created Edit Customer Data View

--- src/controller/ManagerBookingsViewController.java
+++ src/controller/ManagerBookingsViewController.java
@@ -1,7 +1,65 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class ManagerBookingsViewController {
+public class ManagerBookingsViewController implements Initializable {
+	
+	@FXML
+	private Label userLbl2;
 
+	public void GetManager(String user) {
+		// TODO Auto-generated method stub
+		userLbl2.setText(user);
+	}
+	
+	// Manager is able to logout at this page
+	public void SignOut(ActionEvent event) {
+	try {	
+		((Node)event.getSource()).getScene().getWindow().hide();
+		Stage primaryStage = new Stage();
+		FXMLLoader loader = new FXMLLoader();
+		Pane root = loader.load(getClass().getResource("/view/LoginView.fxml").openStream());
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("/application/application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	} catch (Exception e) {
+		
+	}
+	}
+	// Manager is able to go back to overview page to select another option
+	public void toDashboard(ActionEvent event) {
+	try {	
+		((Node)event.getSource()).getScene().getWindow().hide();
+		Stage primaryStage = new Stage();
+		FXMLLoader loader = new FXMLLoader();
+		Pane root = loader.load(getClass().getResource("/view/ManagerRoot.fxml").openStream());
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("/application/application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	} catch (Exception e) {
+		
+	}
+	}
+	@Override
+public void initialize(URL location, ResourceBundle resources) {
+		
+
+
+	}
+
+		
+	
 }
