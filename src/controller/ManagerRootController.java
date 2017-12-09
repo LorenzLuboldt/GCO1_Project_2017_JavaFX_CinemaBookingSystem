b6ebbf06de7feb4241d6944cbb5624152fe0d343
MichaelAring <michael.erik.aring@gmail.com>
2017-12-09 18:45:28 +0000

    Added comments

--- src/controller/ManagerRootController.java
+++ src/controller/ManagerRootController.java
@@ -3,6 +3,7 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,12 +11,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import model.Film;
+import model.Screening;
 
 public class ManagerRootController implements Initializable {
 	@FXML
 	private Label userLbl2;
+	
+	@FXML private ListView<Screening> upcomingScreeningsList; 
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
@@ -24,6 +32,19 @@ public class ManagerRootController implements Initializable {
 	public void GetManager(String user) {
 		// TODO Auto-generated method stub
 		userLbl2.setText(user);
+	}
+	
+	
+	// INCOMPLETE METHOD BELOW
+	// Populates the table in this view 
+	public void FillUpcomingScreeningsListView(ActionEvent event) {
+		//Set up the columns in the table
+		film_title_column.setCellValueFactory(new PropertyValueFactory<Film, String>("filmTitle"));
+		film_description_column.setCellValueFactory(new PropertyValueFactory<Film, String>("filmDescription"));
+
+		//load data from 
+		final ObservableList<Film> filmList = filmDAO.getAllFilms();
+		tableView.setItems(filmList);
 	}
 	
 	//Goes to Movie Overview Page
